3b1ec7a84bbf90ce19a3826017e3a0fe8e9bb262
inspirationjon <ilhomshamidinov55@gmail.com>
2021-05-22 15:37:32 +0500

    Fetch slider

--- src/screens/Slider/Slider.js
+++ src/screens/Slider/Slider.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import './Slider.scss'
+import { useQuery } from 'react-query'
+import { client } from '../../utils/api-client'
 
 function Slider() {
+	const { data: slides, isSuccess } = useQuery({
+		queryKey: 'slides',
+		queryFn: () => client('slide'),
+	})
+
+	isSuccess && console.log(slides)
 	return (
 		<section className='slider p-5'>
 			<form className='slider-from w-50 mb-5 flex-column'>
 				<h2 className='h3 mt-0'>Slider Form</h2>
 				<div className='mb-3 '>
-					<label for='slide_title' class='form-label'>
+					<label htmlFor='slide_title' className='form-label'>
 						Title
 					</label>
 					<input
@@ -18,7 +27,7 @@ function Slider() {
 					/>
 				</div>
 				<div className='mb-3 '>
-					<label for='slide_subtitle' class='form-label'>
+					<label htmlFor='slide_subtitle' className='form-label'>
 						Subtitle
 					</label>
 					<input
@@ -30,7 +39,7 @@ function Slider() {
 					/>
 				</div>
 				<div className='mb-3 '>
-					<label for='slide_file' class='form-label'>
+					<label htmlFor='slide_file' className='form-label'>
 						File
 					</label>
 					<input
@@ -49,26 +58,45 @@ function Slider() {
 			</form>
 
 			<h2 className='h3'>Mavjud Slidelar</h2>
-			<table class='numbers-table table table-striped w-50 m-4'>
+			<table className='numbers-table table table-striped w-50 m-4'>
 				<thead>
 					<tr>
 						<th scope='col'>ID</th>
 						<th scope='col'>Slide sarlavhasi</th>
 						<th scope='col'>Slide ta'rifi</th>
+						<th scope='col'>Slide rasmi</th>
 						<th scope='col'>Slideni o'chirish</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Eshmat</td>
-						<td>Toshmat</td>
-						<td className=' text-decoration-underline cursor-pointer'>
-							<button className='btn btn-danger'>
-								o'chirish
-							</button>
-						</td>
-					</tr>
+					{isSuccess &&
+						slides.map((slide, index) => (
+							<tr>
+								<th scope='row' key={slide.slide_time}>
+									{index + 1}
+								</th>
+								<td>{slide.slide_title}</td>
+								<td>{slide.slide_title}</td>
+								<td>
+									<img
+										className='slide-table__img'
+										src={
+											process.env.REACT_APP_API_URL +
+											'/images/' +
+											slide.slide_img_src
+										}
+										alt={slide.slide_title}
+										width='50'
+										height='50'
+									/>
+								</td>
+								<td className=' text-decoration-underline cursor-pointer'>
+									<button className='btn btn-danger'>
+										o'chirish
+									</button>
+								</td>
+							</tr>
+						))}
 				</tbody>
 			</table>
 		</section>
